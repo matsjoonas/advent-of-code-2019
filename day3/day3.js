df06b4cf3ac0ba4cc42fa6197851553952183c55
matsjoonas <mj@codelight.eu>
2019-12-04 21:54:43 +0200

    Add day1 part1

--- day3/day3.js
+++ day3/day3.js
@@ -1,6 +1,12 @@
 
 const fs = require('fs');
 
+Number.prototype.between = function (a, b) {
+  const min = Math.min.apply(Math, [a, b]);
+  const max = Math.max.apply(Math, [a, b]);
+  return this >= min && this <= max;
+};
+
 function convertTotLineSegments(moves, startingPoint = [0, 0]) {
 
   return moves.reduce((lineSegments, move) => {
@@ -37,12 +43,12 @@ function findSegmentIntersection(segment1, segment2) {
   const vector1 = [segment1[1][0] - segment1[0][0], segment1[1][1] - segment1[0][1]];
   const vector2 = [segment2[1][0] - segment2[0][0], segment2[1][1] - segment2[0][1]];
 
-  function getLineEq(segment)  {
-    if (segment[0][0] === segment[1][0] ) {
+  function getLineEq(segment) {
+    if (segment[0][0] === segment[1][0]) {
       return {
         x: segment[0][0],
       }
-    } else if (segment[0][1] === segment[1][1] ) {
+    } else if (segment[0][1] === segment[1][1]) {
       return {
         y: segment[0][1],
       }
@@ -52,13 +58,33 @@ function findSegmentIntersection(segment1, segment2) {
   const line1 = getLineEq(segment1);
   const line2 = getLineEq(segment2);
 
-  const intersection = [];
-  line1.x !== undefined ? intersection[0] = line1.x : intersection[0] = line2.x;
-  line1.y !== undefined ? intersection[1] = line1.y : intersection[1] = line2.y;
+  let intersection = [];
 
-  console.log(intersection);
-  // find out if the intersection is in the segment
-  // alsho, handle parallel lines
+  if (line1.x !== undefined && line2.x !== undefined) {
+    return intersection;
+  } else if (line1.y !== undefined && line2.y !== undefined) {
+    return intersection;
+  } else {
+    let x, y;
+    if (line1.x !== undefined) {
+      x = line1.x; // line 1 runs parallel to y axis, x varies
+      y = line2.y; // line 2 runs parallel to x axis, y varies
+
+      if (x.between(segment2[0][0], segment2[1][0]) && y.between(segment1[0][1], segment1[1][1])) {
+        intersection = [x, y];
+      }
+
+    } else {
+      x = line2.x; // line 2 runs parallel to y axis 
+      y = line1.y; // line 1 runs parallel to x axis
+      if (x.between(segment1[0][0], segment1[1][0]) && y.between(segment2[0][1], segment2[1][1])) {
+        intersection = [x, y];
+      }
+    }
+
+  }
+
+  return intersection;
 }
 
 fs.readFile('./input.txt', (e, data) => {
@@ -67,9 +93,21 @@ fs.readFile('./input.txt', (e, data) => {
     .map(line => line.split(','))
     .map(moves => convertTotLineSegments(moves));
 
-    
-  findSegmentIntersection([[0, 4], [0, 10]], [[3, 10], [100, 10]]);
-  findSegmentIntersection([[3, 10], [100, 10]], [[0, 4], [0, 10]]);
-  findSegmentIntersection([[5, 4], [5, 10]], [[3, 10], [100, 10]]);
+  const intersections = [];
+  wires[0].forEach(segment => {
+    wires[1].forEach(segment2 => {
+      const intersection = findSegmentIntersection(segment, segment2);
+      if (intersection.length) {
+        intersections.push(intersection);
+      }
+    });
+  });
+
+  const distances = intersections.map(intersection => {
+    return Math.abs(intersection[0]) + Math.abs(intersection[1]);
+  });
+
+
+  console.log(distances.sort((a,b) => a - b)[0]);
 
 });
